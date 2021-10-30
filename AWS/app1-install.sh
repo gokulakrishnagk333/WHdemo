233d4b57956581d7ef4d0838a460847e55c4eb1f
Gokulakrishna <gokulakrishnagk333@gmail.com>
2021-10-30 10:34:04 +0000

    [ci-skip]

--- AWS/app1-install.sh
+++ AWS/app1-install.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 # Instance Identity Metadata Reference - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
 sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl enable httpd
-sudo service httpd start  
-sudo echo '<h1>Welcome to Gokulakrishna - APP-1</h1>' | sudo tee /var/www/html/index.html
-sudo mkdir /var/www/html/app1
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - APP-1</h1> <p>Terraform AWS Option 4 Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+--add-repo \
+https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo docker run --privileged -d --restart=unless-stopped -p 80:31555 --name=app flyahead/apachegokul
+
 

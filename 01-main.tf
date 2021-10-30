d26edcc2d7570b29c95aa8d50e60e0a70e26f4e2
Gokulakrishna <gokulakrishnagk333@gmail.com>
2021-10-30 17:36:43 +0000

    [ci-skip]

--- 01-main.tf
+++ 01-main.tf
@@ -29,7 +29,7 @@ terraform {
 # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     resource_group_name   = "storage-rg"
-    storage_account_name  = "gokulazstorage"
+    storage_account_name  = "gokuldemostorage"
     container_name        = "tfstatefiles"
     key                   = "dev.terraform.tfstate"
   }  

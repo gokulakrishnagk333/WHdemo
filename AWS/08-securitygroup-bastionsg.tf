b813f009880cabe46eb4a795bf1830123124be83
Gokulakrishna <gokulakrishnagk333@gmail.com>
2021-10-30 11:13:20 +0000

    [ci-skip]

--- AWS/08-securitygroup-bastionsg.tf
+++ AWS/08-securitygroup-bastionsg.tf
@@ -4,7 +4,6 @@ module "public_bastion_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   #version = "3.18.0"
   version = "4.0.0"
-
   name = "public-bastion-sg"
   description = "Security Group with SSH port open for everybody (IPv4 CIDR), egress ports are all world open"
   vpc_id = module.vpc.vpc_id

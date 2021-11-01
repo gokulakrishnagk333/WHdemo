d2946e116608f2b3c23ca0319a9ee56a4e246470
Gokulakrishna <47558271+gokulakrishnagk333@users.noreply.github.com>
2021-11-01 10:00:07 +0530

    Update 22-autoscaling-scheduled-actions.tf

--- AWS/22-autoscaling-scheduled-actions.tf
+++ AWS/22-autoscaling-scheduled-actions.tf
@@ -1,21 +1,21 @@
 ## Create Scheduled Actions
 ### Create Scheduled Action-1: Increase capacity during business hours
-resource "aws_autoscaling_schedule" "increase_capacity_7am" {
-  scheduled_action_name  = "increase-capacity-7am"
+resource "aws_autoscaling_schedule" "increase_capacity_8am" {
+  scheduled_action_name  = "increase-capacity-8am"
   min_size               = 2
   max_size               = 4
   desired_capacity       = 2
-  start_time             = "2030-03-30T11:00:00Z" # Time should be provided in UTC Timezone (6AM UTC = 8AM CET)
+  start_time             = "2030-03-30T06:00:00Z" # Time should be provided in UTC Timezone (6AM UTC = 8AM CET)
   recurrence             = "00 06 * * 1-5"
   autoscaling_group_name = aws_autoscaling_group.my_asg.id 
 }
 ### Create Scheduled Action-2: Decrease capacity during business hours
-resource "aws_autoscaling_schedule" "decrease_capacity_5pm" {
-  scheduled_action_name  = "decrease-capacity-5pm"
+resource "aws_autoscaling_schedule" "decrease_capacity_6pm" {
+  scheduled_action_name  = "decrease-capacity-6pm"
   min_size               = 2
   max_size               = 4
   desired_capacity       = 0
-  start_time             = "2030-03-30T21:00:00Z" # Time should be provided in UTC Timezone (4PM UTC = 6PM CET)
+  start_time             = "2030-03-30T16:00:00Z" # Time should be provided in UTC Timezone (4PM UTC = 6PM CET)
   recurrence             = "00 16 * * 1-5"
   autoscaling_group_name = aws_autoscaling_group.my_asg.id
 }
